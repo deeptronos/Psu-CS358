d5659b64ac84a4ef0cda72b5c860b6dd1c5f2d84
Catherine <c.n@pdx.edu>
2025-01-28 17:24:58 -0800

    Drafted domain-specific AST nodes

--- project/interp.py
+++ project/interp.py
@@ -32,9 +32,14 @@ class Rest():
         return f"Rest({self.duration})"
 
 
+@dataclass
+class Tune:
+    t: tuple[Note, ...]
+
+type Value = Tune | int | bool # The return type for Eval
 
 # Arithmetic boolean, binding/variables, equality comparison, relational comparison, conditional
-type Expr = Add | Sub | Mul | Div | Neg | Lit | Let | Name | Or | And | Not | Eq | Lt | If | Note # TODO appropriate inclusion of domain specific additions to Expr?
+type Expr = Add | Sub | Mul | Div | Neg | Lit | Let | Name | Or | And | Not | Eq | Lt | If | ConcatTune | TransposeTune # TODO appropriate inclusion of domain specific additions to Expr?
 
 @dataclass
 class Add():
@@ -143,3 +148,104 @@ class If():
     else_operand: Expr
     def __str__(self) -> str:
         return f"(if {self.bool_operand} then {self.then_operand} else {self.else_operand})"
+
+@dataclass 
+class ConcatTune():
+    left: Tune
+    right: Tune
+    def __str__(self) -> str:
+        return f"(Concatenate {self.left} with {self.right})"
+
+@dataclass
+class TransposeTune:
+    left: Tune
+    right: int # Number of half-steps
+    def __str__(self) -> str:
+        return f"(Transpose {self.left} by {self.right} half-steps)"
+
+
+
+# To interpret let bindings and names, we need environments.
+# We represent environments as lists of bindings, where each binding is a pair of a name and a value.
+# Environments are immutable.
+
+type Binding[V] = tuple[str,V]  # this tuple type is always a pair
+type Env[V] = tuple[Binding[V], ...] # this tuple type has arbitrary length 
+
+from typing import Any
+emptyEnv : Env[Any] = ()  # the empty environment has no bindings
+
+def extendEnv[V](name: str, value: V, env:Env[V]) -> Env[V]:
+    '''Return a new environment that extends the input environment env with a new binding from name to value'''
+    return ((name,value),) + env
+
+def lookupEnv[V](name: str, env: Env[V]) -> (V | None) :
+    '''Return the first value bound to name in the input environment env
+       (or raise an exception if there is no such binding)'''
+    # exercises2b.py shows a different implementation alternative
+    match env:
+        case ((n,v), *rest) :
+            if n == name:
+                return v
+            else:
+                return lookupEnv(name, rest) # type:ignore
+        case _ :
+            return None
+
+
+
+
+# Evaluation produces an integer result, or raises a polite exception if a name is unbound.
+class EvalError(Exception):
+    pass
+
+# The top-level evaluation function just calls the recursive evaluation function with an empty environment.
+def eval(e: Expr) -> Value :
+    return evalInEnv(emptyEnv, e)
+
+
+# Evaluation takes place in an environment, which is threaded through the recursive calls.
+def evalInEnv(env: Env[int], e:Expr) -> int:
+    match e:
+        case Add(l,r):
+            match(evalInEnv(env, l), evalInEnv(env, r)):
+                case(int(lv), int(rv)):
+                    return lv + rv
+                case _:
+                    raise EvalError("addition of non-integers")
+        case Sub(l,r):
+             match(evalInEnv(env, l), evalInEnv(env, r)):
+                case(int(lv), int(rv)):
+                    return lv - rv
+                case _:
+                    raise EvalError("subtraction of non-integers")
+        case Mul(l,r): # we can fix the evaluation order or l and r more explicitly if we care
+            match (evalInEnv(env,l), evalInEnv(env,r)):
+                case (int(lv), int(rv)):
+                    return lv * rv
+                case _:
+                    raise EvalError("multiplication of non-integers")
+        case Neg(s):
+            match evalInEnv(env,s):
+                case int(i):
+                    return -i
+                case _:
+                    raise EvalError("negation of non-integer")
+        case(Lit(lit)):
+            match lit:  # two-level matching keeps type-checker happy
+                case int(i):
+                    return i
+                case bool(i):
+                    return i
+                case Note(_) as a:
+                    return Tune((a,))
+          
+        case Name(n):
+            v = lookupEnv(n, env)
+            if v is None:
+                raise EvalError(f"unbound name {n}")
+            return v
+        case Let(n,d,b):
+            v = evalInEnv(env, d)
+            newEnv = extendEnv(n, v, env)
+            return evalInEnv(newEnv, b)
